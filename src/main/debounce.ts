1a5210ee6e752b52b640a6e8fce95397db496c52
Peter Rybar <pr.rybar@gmail.com>
2018-09-04 14:32:16 +0200

    debounce - implemented

---
+++ src/main/debounce.ts
@@ -0,0 +1,55 @@
+export function debounce<F extends (...args: any[]) => void>(func: F, delay: number = 300) {
+    let timeout: number;
+    return function (this: any, ...args: F extends (...args: infer X) => void ? X : never) {
+        clearTimeout(timeout);
+        const context = this;
+        timeout = setTimeout(function () {
+            func.apply(context, args);
+        }, delay);
+    };
+}
+
+// Decorator
+export function debounceFnc(delay: number = 300) {
+    return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+        const originalMethod = descriptor.value;
+        descriptor.value = debounce(originalMethod, delay);
+        return descriptor;
+    };
+}
+
+
+// TEST -----------------------------------------------------------------------
+
+// const d = debounce(function (this: any, x: string, y: number) {
+//     console.log("debounce", x, y);
+// }, 300);
+// d("peter", 3);
+
+// const o = {
+//     x: "o.x",
+//     m: function (y: number) {
+//         console.log("o.m", this.x, y);
+//     },
+//     d: ({} as any)
+// };
+// o.d = debounce(o.m, 300);
+// o.d("peter", 3);
+
+// class O {
+//     x:string = "O.x";
+
+//     @debounceFnc(300)
+//     m(y: string) {
+//         console.log("O.m", this.x, y);
+//     }
+// }
+// const obj = new O();
+// // obj.m = debounce(obj.m, 300); // decorator equivalent
+// obj.m("m(p)");
+
+// setTimeout(() => { obj.m("m(p)") }, 200);
+// setTimeout(() => { obj.m("m(p)") }, 1200);
+// setTimeout(() => { obj.m("m(p)") }, 1400);
+// setTimeout(() => { obj.m("m(p)") }, 2200);
+// setTimeout(() => { obj.m("m(p)") }, 2400);
