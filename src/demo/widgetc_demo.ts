097ae014d465ca34a1eda0fea8c48637bdd8e71f
Peter Rybar <pr.rybar@gmail.com>
2018-10-24 22:15:01 +0200

    widget - component demo improved

--- src/demo/widgetc_demo.ts
+++ src/demo/widgetc_demo.ts
@@ -10,23 +10,19 @@ interface PageState {
 function page(state: PageState, events: Events): JsonMLs {
     return [
         ["h2", state.title],
-        ["p", state.count.toString()],
-        button<number>({ label: "-", event: { type: "dec", data: 2 } }, events),
-        button<number>({ label: "+", event: { type: "inc", data: 2 } }, events)
+        ["p",
+            ["em", "Count"], ": ", state.count.toString(),
+            " ",
+            button("-", () =>  events.emit("dec", 1)),
+            button("+", () => events.emit("inc", 2)),
+            " ",
+            button("xxx", () => events.emit("xxx")),
+        ],
     ];
 }
 
-interface ButtonState<T> {
-    label: string;
-    event: {
-        type: string;
-        data: T;
-    };
-}
-
-function button<T>(state: ButtonState<T>, events: Events): JsonML {
-    const { type, data } = state.event;
-    return ["button", { click: () => events.emit(type, data) }, state.label];
+function button(label: string, cb: (e: Event) => void): JsonML {
+    return ["button", { click: cb }, label];
 }
 
 const app = new WidgetC<PageState>({ title: "Counter", count: 77 }, page);
@@ -36,33 +32,33 @@ app.events
     .on("", (data, widget, event) => {
         console.log("event:", data, event, widget);
     })
-    .on("inc", (num, app) => {
-        app.events.emit("dec", 1);
+    .on("inc", (num, widget) => {
+        widget.events.emit("dec", 1);
     })
-    .on("inc", (num, app) => {
-        app.state.count += num;
-        app.update();
+    .on("inc", (num, widget) => {
+        widget.state.count += num;
+        widget.update();
     })
-    .on("dec", (num, app) => {
-        const s = app.state;
+    .on("dec", (num, widget) => {
+        const s = widget.state;
         s.count -= num;
-        app.state = s;
+        widget.state = s;
     });
     // .cbs(
     //     {
-    //         inc: (num, app) => {
-    //             app.events.emit("dec", 1);
+    //         inc: (num, widget) => {
+    //             widget.events.emit("dec", 1);
     //         },
-    //         dec: (num, app) => {
-    //             const s = app.state;
+    //         dec: (num, widget) => {
+    //             const s = widget.state;
     //             s.count -= num;
-    //             app.state = s;
+    //             widget.state = s;
     //         }
     //     },
     //     {
-    //         inc: (num, app) => {
-    //             app.state.count += num;
-    //             app.update();
+    //         inc: (num, widget) => {
+    //             widget.state.count += num;
+    //             widget.update();
     //         }
     //     }
     // );
