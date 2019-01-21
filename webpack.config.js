159206067b22d7d64bcefac95ff9d49ff19974c6
Peter Rybar <pr.rybar@gmail.com>
2019-01-21 11:01:44 +0100

    uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const glob = require("glob");
 const { CheckerPlugin } = require("awesome-typescript-loader");
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 const entries = {
     ...glob.sync('./src/*.ts')
@@ -31,9 +32,21 @@ module.exports = {
     // devtool: "cheap-eval-source-map",
     // devtool: "hidden-source-map",
     // devtool: "nosources-source-map",
-    entry: entries,
     // entry: {
     //     index: "./src/index.ts"
+    // },
+    entry: entries,
+    // optimization: {
+    //     minimizer: [
+    //         new UglifyJsPlugin({
+    //             uglifyOptions: {
+    //                 output: {
+    //                     comments: false
+    //                 },
+    //             },
+    //             extractComments: true, // /(?:^!|@(?:license|preserve))/i
+    //         })
+    //     ]
     // },
     output: {
         // filename: '[name].[chunkhash].js',
