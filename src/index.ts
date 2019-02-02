2333843b8c88d37edc745c260849887000d3aa50
Peter Rybar <pr.rybar@gmail.com>
2019-02-03 00:16:28 +0100

    jsonml removed, replaced by hsml

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,14 @@ import * as encode from "./encode";
 import * as form from "./form";
 import * as hash from "./hash";
 import * as history from "./history";
-import * as hsmlXWidget from "./hsml-xwidget";
 import * as hsmlDom from "./hsml-dom";
 import * as hsmlHtml from "./hsml-html";
 import * as hsmlIDom from "./hsml-idom";
 import * as hsmlWidget from "./hsml-widget";
+import * as hsmlXWidget from "./hsml-xwidget";
+import * as hsmlAWidget from "./hsml-awidget";
 import * as hsml from "./hsml";
 import * as http from "./http";
-import * as jsonml from "./jsonml";
-import * as jsonmlHtml from "./jsonml-html";
-import * as jsonmlDom from "./jsonml-dom";
-import * as jsonmlIDom from "./jsonml-idom";
 import * as load from "./load";
 import * as objpaths from "./objpaths";
 import * as router from "./router";
@@ -22,11 +19,6 @@ import * as signal from "./signal";
 import * as template from "./template";
 import * as tmpl from "./tmpl";
 import * as validators from "./validators";
-import * as widgetNode from "./widget-node";
-import * as widget from "./widget";
-import * as widgeta from "./widgeta";
-import * as widgetc from "./widgetc";
-import * as widgete from "./widgete";
 
 export {
     debounce,
@@ -35,27 +27,19 @@ export {
     form,
     hash,
     history,
-    hsmlXWidget,
     hsmlDom,
     hsmlHtml,
     hsmlIDom,
     hsmlWidget,
+    hsmlXWidget,
+    hsmlAWidget,
     hsml,
     http,
-    jsonml,
-    jsonmlHtml,
-    jsonmlDom,
-    jsonmlIDom,
     load,
     objpaths,
     router,
     signal,
     template,
     tmpl,
-    validators,
-    widgetNode,
-    widget,
-    widgeta,
-    widgetc,
-    widgete
+    validators
 };
